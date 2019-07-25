af3e412a5c7f0228fca224366309833cc815afbc
PoonamShendge24 <53131194+PoonamShendge24@users.noreply.github.com>
2019-07-25 18:35:27 +0530

    Add files via upload

---
+++ api/dhl.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const tabletojson = require('tabletojson');
+const axios = require('axios')
+
+router.get('/:awb', (req, res) => {
+    let trackingId = req.params.awb
+	let url = `https://www.dhl.co.in/en/express/tracking.html?AWB=${trackingId}`;
+	console.log("axios url is"+url);
+    return axios.get(url)
+    .then(response => {
+		console.log("data is"+response.data);
+        if(response.data[0].activityType == "Invalid Data"){
+            return res.json({ result: `Invalid Tracking No. ${trackingId}` })
+        }
+        let final = response.data.reduce((acc, current) => [...acc, { location: `${current.origin}`, detail: `${current.activityType}`.replace(/<\/?[^>]+(>|$)/g, ""),  status: `${current.deliveryStatus}`, date: `${current.dateWithNoSuffix}`, time: `${current.time}` }],[]);
+        return res.json({ result: final })
+    })
+    .catch(err => {
+        return res.json({
+            error: err
+        })
+    })
+})
+
+module.exports = router;
